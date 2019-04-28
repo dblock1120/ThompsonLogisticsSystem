04bae2ffba40522438b510707fb50b0d13a9d64e
Darlene Block <dar.block@gmail.com>
2019-04-28 16:39:12 -0500

    initial commit

---
+++ ThompsonLogisticsSystem/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+using Microsoft.AspNetCore.Mvc;
+using ThompsonLogisticsSystem.Models;
+
+namespace ThompsonLogisticsSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public IActionResult Purchasing()
+        {
+            ViewData["Message"] = "Welcome to the Purchasing Hub!";
+
+            return View();
+        }
+
+        public IActionResult Sales()
+        {
+            ViewData["Message"] = "Welcome to the Sales Hub!";
+
+            return View();
+        }
+
+        public IActionResult Operations()
+        {
+            ViewData["Message"] = "Welcome to the Operations Hub!";
+
+            return View();
+        }
+
+        public IActionResult Admin()
+        {
+            ViewData["Message"] = "Welcome to System Administration!";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
