2c858dfda435b3e09841bd2339f510d53f1ff19b
Joe Arcelo <josepharcelo@gmail.com>
2019-04-30 12:07:02 -0500

    Added Sale controller and corresponding views

---
+++ ThompsonLogisticsSystem/Controllers/SaleController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+using Microsoft.AspNetCore.Mvc;
+using ThompsonLogisticsSystem.Models;
+
+namespace ThompsonLogisticsSystem.Controllers
+{
+    public class SaleController : Controller
+    {
+       
+
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public IActionResult CreateSalesOrder()
+        {
+            ViewData["Message"] = "Create Sales Order.";
+
+            return View();
+        }
+
+        public IActionResult ViewSalesOrders()
+        {
+            ViewData["Message"] = "My Sales Orders.";
+            return View();
+        }
+
+        public IActionResult UpdateSalesOrder()
+        {
+            ViewData["Message"] = "Update Sales Order.";
+            return View();
+        }
+
+        public IActionResult AddNewCustomer()
+        {
+            ViewData["Message"] = "Add New Customer.";
+            return View();
+        }
+
+        public IActionResult UpdateCustomer()
+        {
+            ViewData["Message"] = "Update Customer Info.";
+            return View();
+        }
+
+        public IActionResult GenerateReport()
+        {
+            ViewData["Message"] = "Sales Report.";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
