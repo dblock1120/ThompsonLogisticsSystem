5ebb993e5062920ed445a4134dbe64c571835646
Joe Arcelo <josepharcelo@gmail.com>
2019-04-30 00:49:02 -0500

    Added purchasing controller to project

---
+++ ThompsonLogisticsSystem/Controllers/PurchasingController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+using Microsoft.AspNetCore.Mvc;
+using ThompsonLogisticsSystem.Models;
+
+namespace ThompsonLogisticsSystem.Controllers
+{
+    public class PurchasingController : Controller
+    {
+       
+
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public IActionResult CreatePurchaseOrder()
+        {
+            ViewData["Message"] = "Please select a vendor to start creating a new purchase order.";
+
+            return View();
+        }
+               
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
